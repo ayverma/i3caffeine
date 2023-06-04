88c2d56fd7eff31c4c9734a58321825cdd23cdfd
Ayush verma <ashv333@gmail.com>
2023-06-04 23:04:11 +0530

    Update install_dependencies.sh
    updated package manager and repository selection based on OS and distro detection

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-pacman -Scqy --noconfirm --noprogressbar xorg-xset
 #To download packages, clean local cache, quiet install, and refresh database
+base_os = `grep '^ID_LIKE' /etc/os-release`
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [[ $base_os == *arch ]]; then
+        echo "Arch-based OS detected"
+        echo "Installing required packages for arch based systems from Official Arch repositories..."
+        pacman -Scq --noconfirm --noprogressbar xorg-xset
+        #To download packages, clean local cache, quiet install, and refresh database
+
+    elif [[ $base_os == *debian ]]; then
+        echo "Debian-based OS detected"
+        apt-get -y install x11-xserver-utils
+    # more distros to be added
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # similar installation process for macOS
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # similar installation process with Windows packages
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # similar installation for freeBSD package manager
+else
+        # Unknown.
+fi
+
+
